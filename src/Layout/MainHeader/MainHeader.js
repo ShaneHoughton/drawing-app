d207bbe8b7388cdb94fbad5b8d0550a3eb382fff
Shane Houghton <houghtons@moravian.edu>
2023-08-06 21:53:06 -0400

    fix main header to clear all posts when directing to home

--- src/Layout/MainHeader/MainHeader.js
+++ src/Layout/MainHeader/MainHeader.js
@@ -9,11 +9,14 @@ import {useState, useEffect} from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 import Notification from '../Notification/NotificationBanner';
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../../store/ui';
 
 
 const MainHeader = (props) => {
   const [signedIn, setSignedIn] = useState(auth.currentUser !== null);
-  const navigate = useNavigate();
+  const navigate = useNavigate()
+  const dispatch = useDispatch();
   useEffect(()=>{
     const updateSignedIn = auth.onAuthStateChanged((user) => {
       setSignedIn(user !== null && user.emailVerified);
@@ -45,7 +48,12 @@ const MainHeader = (props) => {
   return (
     <>
     <header className={classes.header}>
-      <h1 onClick={()=>{navigate('/')}} style={{cursor:'pointer'}}>Sketchi.io</h1>
+      <h1 
+      onClick={()=>{
+        navigate('/');
+        dispatch(uiActions.clearPosts());
+      }}
+       style={{cursor:'pointer'}}>Sketchi.io</h1>
       <nav>
         <ul>
           <li>
